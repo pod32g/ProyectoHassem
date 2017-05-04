df6de67bdd80ff8e16268d709be99ddb20f38631
NakataVega <vegagerardo96@gmail.com>
2017-05-04 12:24:40 -0500

    vega

--- db.py
+++ db.py
@@ -1,8 +1,24 @@
 import MySQLdb
 
-conexion = MySQLdb.connect('localhost','root','Lizbeth','ProyectoHassem')
+def consulta(sentencia):
+	lista = []
+	conexion = MySQLdb.connect(#host,usuario,password,database)
+	cursor = conexion.cursor()
+	cursor.execute(sentencia)
+	filas = cursor.fetchall() #fetchall() extrae todos los registros que cumplen la condicion especificada
+	for fila in filas:
+		fila = list(fila)
+		for element in fila:
+			if(type(element) == long):
+				element = int(element)
+		fila = tuple(fila)
+		lista.append(fila)
+	conexion.commit()
+	conexion.close()
+	return lista
 
-cursor = conexion.cursor()
+sentencia = "SELECT * FROM test WHERE ciudad = 'Guadalajara';"
+datos = consulta(sentencia)
 
-sentencia = 'SELECT * FROM Tabla1;'
-cursor.execute(sentencia)
+datos = consulta(sentencia)
+print(datos)
