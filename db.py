f7b9a75b6b08af0234f3ca28edb9f6ed77fe1b80
NakataVega <vegagerardo96@gmail.com>
2017-05-07 20:48:54 -0500

    vega

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 import MySQLdb
-
+#Funcion que retorna en una lista todos los registros de la tabla
 def consulta(sentencia):
 	lista = []
-	conexion = MySQLdb.connect('host','usuario','password','database')
+	conexion = MySQLdb.connect('localhost','root','Lizbeth','ProyectoHassem')
 	cursor = conexion.cursor()
 	cursor.execute(sentencia)
 	filas = cursor.fetchall() #fetchall() extrae todos los registros que cumplen la condicion especificada
@@ -17,8 +17,21 @@ def consulta(sentencia):
 	conexion.close()
 	return lista
 
-sentencia = "SELECT * FROM test WHERE ciudad = 'Guadalajara';"
-datos = consulta(sentencia)
+#sentencia = "SELECT * FROM test WHERE ciudad = 'Guadalajara';"
+#datos = consulta(sentencia)
 
-datos = consulta(sentencia)
-print(datos)
+#datos = consulta(sentencia)
+#print(datos)
+
+#Funcion que verifica si un regisrtro ya existe
+def existe(tabla,atributo,valor):
+	sentencia = "SELECT * FROM " + tabla + " WHERE " + atributo + " = " + valor + ";"
+	print(sentencia)
+	conexion = MySQLdb.connect('localhost','root','Lizbeth','ProyectoHassem')
+	cursor = conexion.cursor()
+	cursor.execute(sentencia)
+	conexion.commit()
+	conexion.close()
+	print obtenido #Quiero saber el valor obtenido para saber que retornar, pero no llega hasta aqui men, truena en el execute
+
+existe('test','nombre','Jorge')
